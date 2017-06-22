a659213d6e6638ae5c84d963ba2e48c366a0a1ac
shaya <shayasms128@gmail.com>
2017-06-22 11:51:53 +0300

    remove redundant requires in server

--- server/server.js
+++ server/server.js
@@ -1,25 +1,10 @@
 require('./config/config');
-var path = require('path'),
-
-  fs = require('fs'),
-  os = require('os'),
-  formidable = require('formidable'),
-  gm = require('gm'),
-  s3 = require('./amazon/s3.js');
 
 
-const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 var methodOverride = require('method-override');
-const {ObjectID} = require('mongodb');
-
 var {mongoose} = require('./db/mongoose');
-var {Todo} = require('./models/todo');
-var {Group} = require('./models/group');
-var {Lift} = require('./models/lift');
-var {User} = require('./models/user');
-var {authenticate} = require('./middleware/authenticate');
 var routes = require('./routes/routes')
 var app = express();
 const port = process.env.PORT;
