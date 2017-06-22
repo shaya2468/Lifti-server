c56fd03c0e49ef2179524697d5b2cd33c28e1f0c
shaya <shayasms128@gmail.com>
2017-06-22 13:37:09 +0300

    add validation of uniqueness of perm, in the future will add compound index that for some reason isn’t working

--- server/controllers/perm_controller.js
+++ server/controllers/perm_controller.js
@@ -5,16 +5,29 @@ const _ = require('lodash');
 module.exports = {
   create(req, res) {
 
-    let perm = new Perm({
-      _group: req.body.group_id,
-      _applicant: req.user._id,
-      message: req.body.message
-    });
+    //Todo : make 2 indexes unique, this is very bad hack, because it makes 2 queries to the db, right
+    // now the 2 index uniqueness doesn't work, need investigation.
+    Perm.find({_group: req.body.group_id, _applicant: req.user._id})
+      .then((res) => {
+        console.log(res);
+        console.log(res.length);
+        if (res.length > 0){
+          throw({message: "request already exists"})
+        }
 
-    perm.save().then((doc) => {
-      res.send({});
-    }, (e) => {
-      res.status(400).send(e);
-    });
+        let perm = new Perm({
+          _group: req.body.group_id,
+          _applicant: req.user._id,
+          message: req.body.message
+        });
+
+        return perm.save();
+
+      }).then((result) => {
+          res.send({});
+      }).catch((e) => {
+        console.log(e);
+        res.status(400).send(e);
+      })
   }
 }
